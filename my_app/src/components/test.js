2cc00514793cf59ef884cc890731540c3e7fdb68
קולינקו רחל < @m21.local>
2024-12-11 14:14:03 +0200

    start

---
+++ my_app/src/components/test.js
@@ -0,0 +1,11 @@
+import Welcome from "./welcome"
+function Test(){
+    return(
+        <>
+        <h2>test</h2>
+        <Welcome></Welcome>
+        </>
+        
+    )
+}
+export default Test;
---
+++ my_app/src/components/welcome.js
@@ -0,0 +1,12 @@
+import logo from '../assests/logo512.png'
+function Welcome(){
+    return(
+        <>
+        <h2 >Welcome</h2>
+        <img src={logo}></img>
+        </>
+        
+
+    )
+}
+export default Welcome;
